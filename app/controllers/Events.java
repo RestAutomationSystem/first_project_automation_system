ce0ff73da38a2b757f2539fb90d90e047c368826
Ualikhan <togay.ualihan@gmail.com>
2015-08-04 14:18:18 +0600

    Included log event view

---
+++ app/controllers/Events.java
@@ -0,0 +1,31 @@
+package controllers;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Администратор
+ * Date: 04.08.15
+ * Time: 9:19
+ * To change this template use File | Settings | File Templates.
+ */
+
+import play.*;
+import models.*;
+import play.mvc.*;
+import play.data.*;
+import static play.data.Form.*;
+import views.html.event.*;
+
+@Security.Authenticated(Secured.class)
+public class Events  extends Controller {
+
+    public static Result index(int page, String sortBy, String order, String filter) {
+        return ok(
+                index.render(
+                        User.find.where().eq("email", request().username()).findUnique(),
+                        Event.page(page, 10, "event_time", "desc", filter),
+                        sortBy, order, filter
+                )
+        );
+    }
+
+}
--- app/models/Event.java
+++ app/models/Event.java
@@ -1,7 +1,7 @@
 package models;
 
 import java.util.*;
-
+import com.avaje.ebean.*;
 import javax.persistence.*;
 import play.data.format.*;
 import play.data.validation.Constraints.Required;
@@ -13,21 +13,21 @@ public class Event extends Model{
     @Id
     @Version
     @GeneratedValue(strategy=GenerationType.IDENTITY)
-    public int id;
+    private int id;
 
-    public String event_type;
+    private String event_type;
 
     @Column(columnDefinition="TEXT")
-    public String description;
+    private String description;
 
-    public String old_value;
-    public String new_value;
+    private String old_value;
+    private String new_value;
 
     @Formats.DateTime(pattern="dd/MM/yyyy HH:mm:ss")
-    public Date event_time;
+    private Date event_time;
 
     @ManyToOne
-    public User author;
+    private User author;
 
     public Event(String event_type,String description,String old_value,String new_value,Date event_time,User author){
         this.event_type=event_type;
@@ -43,6 +43,17 @@ public class Event extends Model{
 
     public static List<Event> findAll() {
         return find.all();
+    }
+
+    public static Page<Event> page(int page, int pageSize, String sortBy, String order, String filter) {
+        return
+                find.where()
+                        .ilike("event_type", "%" + filter + "%")
+                        .orderBy(sortBy + " " + order)
+                        .fetch("author")
+                        .findPagingList(pageSize)
+                        .setFetchAhead(false)
+                        .getPage(page);
     }
 
 
@@ -64,5 +75,61 @@ public class Event extends Model{
     }
     public static void delete(int id){
         find.ref(id).delete();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEvent_type() {
+        return event_type;
+    }
+
+    public void setEvent_type(String event_type) {
+        this.event_type = event_type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getOld_value() {
+        return old_value;
+    }
+
+    public void setOld_value(String old_value) {
+        this.old_value = old_value;
+    }
+
+    public String getNew_value() {
+        return new_value;
+    }
+
+    public void setNew_value(String new_value) {
+        this.new_value = new_value;
+    }
+
+    public Date getEvent_time() {
+        return event_time;
+    }
+
+    public void setEvent_time(Date event_time) {
+        this.event_time = event_time;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
     }
 }
